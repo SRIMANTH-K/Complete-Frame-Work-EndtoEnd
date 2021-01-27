5161755a197df094e1526a5369b867f2b4924666
srimanth <srimanthk65@gmail.com>
2021-01-27 16:57:24 +0530

    this is done by srimanth

---
+++ src/test/java/examples/Framewrokexample.java
@@ -0,0 +1,12 @@
+package examples;
+
+public class Framewrokexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is done my srimanth");
+		System.out.println("From India IST time zone");
+	}
+
+}
