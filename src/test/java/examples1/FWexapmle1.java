cda74f03cf3d2a2b0a1c0e77e84d3a01b8c086a4
srimanth <srimanthk65@gmail.com>
2021-01-27 17:16:15 +0530

    this is done by jonas

---
+++ src/test/java/examples1/FWexapmle1.java
@@ -0,0 +1,11 @@
+package examples1;
+
+public class FWexapmle1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is done my jonas khanwald");
+		System.out.println("From India IST time zone");
+	}
+
+}
